31d786fdfc032ac4509d4c65d3213d15f08ffcb1
Andriy Miranskyy <avm@ryerson.ca>
2022-03-28 12:18:14 -0400

    Create new todo

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label>Description</label><br>
+        <input v-model="todo.description" type="text" required />
+      </div>
+      <div>
+        <label>Owner</label><br>
+        <input v-model="todo.owner" type="text" />
+      </div>
+      <button type="submit"> Create </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { createTodo } from '@/todo.db'
+import { reactive } from 'vue'
+
+const todo = reactive({ description: '', owner: '' })
+
+const onSubmit = async () => {
+  await createTodo({ ...todo })
+  todo.description = ''
+  todo.owner = ''
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h1>Create ToDo</h1>
+    <CreateTodo/>
     <h1>List ToDos</h1>
     <ShowTodos/>
   </div>
@@ -7,11 +9,13 @@
 
 <script>
 // @ is an alias to /src
+import CreateTodo from "@/components/CreateTodo.vue"
 import ShowTodos from "@/components/ShowTodos.vue"
 
 export default {
   name: 'HomeView',
   components: {
+    CreateTodo,
     ShowTodos
   }
 }
