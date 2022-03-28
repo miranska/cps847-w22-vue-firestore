e810aa4cccaa642a749ef2c9bfc079eae5d12bf2
Andriy Miranskyy <avm@ryerson.ca>
2022-03-28 12:15:27 -0400

    Show all todos

---
+++ src/components/ShowTodos.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>Description</th>
+        <th>Owner</th>
+        <th></th>
+      </tr>
+      <tr v-for="{ id, description, owner } in todos" :key="id">
+          <td>{{ description }}</td>
+          <td>{{ owner }}</td>
+          <td>
+            <router-link :to="`/update/${id}`">
+              <button> Update </button>
+            </router-link>
+            <button @click="deleteTodo(id)"> Delete </button>
+          </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { readTodos, deleteTodo } from '@/todo.db'
+const todos = readTodos()
+</script>
+
+<!-- The <script setup> code is equivalent to the following:
+
+<script>
+import { readTodos, deleteTodo } from '@/todo.db'
+
+export default {
+  name: 'ShowUsers',
+  setup() {
+    const todos = readTodos()
+    return { todos, deleteTodo }
+  }
+}
+</script>
+
+-->
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>List ToDos</h1>
+    <ShowTodos/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ShowTodos from "@/components/ShowTodos.vue"
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ShowTodos
   }
 }
 </script>
